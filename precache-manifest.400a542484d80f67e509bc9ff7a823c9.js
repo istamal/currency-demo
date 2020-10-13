ba82d08b333b77df195e1617d23ada7ccfcec6b8
istamal <dagalaev@gmail.com>
2020-10-13 09:10:45 +0300

    Updates

---
+++ precache-manifest.400a542484d80f67e509bc9ff7a823c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060ecef490e618b5e533f6f646aa5e41",
+    "url": "/currency-demo/index.html"
+  },
+  {
+    "revision": "dbfb1ccebcc650cf0a5f",
+    "url": "/currency-demo/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "1cdd3412353a3f78470a",
+    "url": "/currency-demo/static/css/main.da6663f7.chunk.css"
+  },
+  {
+    "revision": "dbfb1ccebcc650cf0a5f",
+    "url": "/currency-demo/static/js/2.cd776555.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/currency-demo/static/js/2.cd776555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cdd3412353a3f78470a",
+    "url": "/currency-demo/static/js/main.8fa4610c.chunk.js"
+  },
+  {
+    "revision": "bb3ce78c1ac565a5641e",
+    "url": "/currency-demo/static/js/runtime-main.80911f59.js"
+  }
+]);
